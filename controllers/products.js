751f5655a7e2070b6c481bda4d8ee6c18e953b28
Mile Mijatović <mile@milemijatovic.com>
2019-02-22 11:31:05 +0100

    MVC update

--- controllers/products.js
+++ controllers/products.js
@@ -20,17 +20,19 @@ exports.postAddProducts = (req, res) => {
     // Push a new object (product title and description) into products array
     const product = new Product(req.body.title, req.body.description);
     product.save();
+
     // Redirects to the URL derived from the specified path (home page)
     res.redirect('/');  
 };
 
 // Get products controller (shop page)
 exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll();
-    res.render('shop', { 
-        pageTitle: 'Home page',
-        // Create prods array and store product data into our template, so we can use and output it in the shop template
-        prods: products,
-        pageId: '/'
-    })
+    Product.fetchAll((products) => {
+        res.render('shop', { 
+            pageTitle: 'Home page',
+            // Create prods array and store product data into our template, so we can use and output it in the shop template
+            prods: products,
+            pageId: '/'
+        })
+    });
 }
--- models/product.js
+++ models/product.js
@@ -1,17 +1,59 @@
-// Create constant "products" where we will receive new products as an array
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
+// In this class I want to find the shape of a single shop product
 module.exports = class Product {
+    /**
+     * I want to receive a title and description of the product which will then create from inside my controller.
+     * This allows me to create an object on this class where I can pass the title and description to the constructor 
+     * which we call with "new Product"
+     * @param {*} productTitle product title property
+     * @param {*} productDescription product description property
+     */
     constructor(productTitle, productDescription) {
         this.title = productTitle;
         this.description = productDescription;
     }
-    // Save Method
+    
+    /**
+     * With save() method we will store our product into data folder
+     */
     save() {
-        products.push(this);
+        // Get the application path to the data folder, and product.json file into that folder
+        const p = path.join(
+            path.dirname(process.mainModule.filename), 
+            'data', 
+            'products.json'
+        );
+        fs.readFile(p, (err, data) => {
+            let products = [];
+            // If there is no error
+            if (!err) {
+                products = JSON.parse(data);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll() {
-        return products;
+    /**
+     * Retrieve products from an array (fetch data)
+     * 
+     * static -> make sure that I can call this method directly on the Product class itself and not on an instantiated object
+     */
+    static fetchAll(callback) {
+        const p = path.join(
+            path.dirname(process.mainModule.filename), 
+            'data', 
+            'products.json'
+        );
+        fs.readFile(p, (err, data) => {
+            if (err) {
+                callback([]);
+            }
+            callback(JSON.parse(data));
+        })
     }
 }
